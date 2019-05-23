cbc2e6bc488dccc9ba65ce4060e6a9b5d29a3a0c
ThisIsFix <50284031+ThisIsFix@users.noreply.github.com>
2019-05-23 15:39:28 -0500

    Add files via upload

---
+++ BillboardFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+	public Transform camTransform;
+
+	Quaternion originalRotation;
+
+    void Start()
+    {
+        originalRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+     	transform.rotation = camTransform.rotation * originalRotation;   
+    }
+}
